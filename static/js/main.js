9a2a49728912376896c9c99bffbeef1c63112f1e
phantamanta44 <evandalong@gmail.com>
2016-02-12 16:34:40 +0000

    Styling and structure

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,18 @@
-
+$(document).ready(function() {
+    
+    $('.ec-button').click(function() {
+        setTimeout(function() {
+            $('.ec-button').each(function (i, element) {
+                var obj = $(element);
+                if (obj.parent().attr('aria-expanded') === 'true') {
+                    obj.removeClass('mdi-plus');
+                    obj.addClass('mdi-minus');
+                } else {
+                    obj.removeClass('mdi-minus');
+                    obj.addClass('mdi-plus');
+                }
+            });
+        }, 1);
+    });
+    
+})
